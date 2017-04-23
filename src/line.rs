364a4f35d8ae12cce7dd8de19cf8f3e084024e45
Andrew Mason <andrew@fixedpoint.xyz>
2017-04-23 13:17:18 -0400

    Updates Line interface/parsing to match current backend protocol

--- src/line.rs
+++ src/line.rs
@@ -1,32 +1,34 @@
 use serde_json;
 
 pub struct Line {
-    pub text: String,
-    pub selection: Option<(u64, u64)>,
-    cursor: Option<u64>,
+    text: Option<String>,
+    cursor: Option<Vec<u64>>,
+    styles: Option<Vec<i64>>,
 }
 
 impl Line {
     pub fn from_value(value: &serde_json::Value) -> Line {
-        let line_arr = value.as_array().unwrap();
-        let mut line = Line {
-            text: line_arr[0].as_str().unwrap().to_string(),
-            cursor: None,
-            selection: None,
+        let obj = value.as_object().unwrap();
+        let text = match obj.get("text") {
+            Some(text) => Some(text.as_str().unwrap().to_string()),
+            _ => None,
         };
-        for annotation in line_arr.iter().skip(1).map(|a| a.as_array().unwrap()) {
-            match annotation[0].as_str().unwrap() {
-                "cursor" => {
-                    line.cursor = Some(annotation[1].as_u64().unwrap());
-                },
-                "sel" => {
-                    line.selection = Some((annotation[1].as_u64().unwrap(), annotation[2].as_u64().unwrap()));
-                },
-                _ => {
-                    error!("unknown annotation");
-                }
-            }
+        let cursor = match obj.get("cursor") {
+            Some(cursor) => {
+                Some(cursor.as_array().unwrap().iter().map(|c| c.as_u64().unwrap()).collect())
+            },
+            _ => None,
+        };
+        let styles = match obj.get("styles") {
+            Some(styles) => {
+                Some(styles.as_array().unwrap().iter().map(|s| s.as_i64().unwrap()).collect())
+            },
+            _ => None,
+        };
+        Line {
+            text: text,
+            cursor: cursor,
+            styles: styles,
         }
-        line
     }
 }
