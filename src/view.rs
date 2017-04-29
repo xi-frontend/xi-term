xi-frontend/xi-term

commit 2d581e830dbd95de1c3fe8d66f0ca93b6bad9ba2

use serde_derive for deserialization

src/view.rs
@@ -21,11 +21,12 @@ impl View {
         if self.last_rev > update.rev {
             return;
         }
+
         let mut lines = vec![];
         let mut index = 0;
 
-        for op in &update.ops {
-            index = op.apply(&self.lines, index, &mut lines);
+        for operation in &update.operations {
+            index = operation.apply(&self.lines, index, &mut lines);
         }
 
         self.last_rev = update.rev;

Cargo.toml
@@ -12,6 +12,8 @@ clippy = {version = "*", optional = true}
 termion = "*"
 jsonrpc-core = "*"
 serde_json = "*"
+serde = "*"
+serde_derive = "*"
 log = "0.3"
 clap = "2.0"
 

src/core.rs
@@ -24,6 +24,7 @@ pub struct Core {
 }
 
 impl Core {
+    #[cfg_attr(rustfmt, rustfmt_skip)]
     pub fn new(executable: &str, file: &str) -> Core {
         // spawn the core process
         let process = Command::new(executable)
@@ -38,40 +39,50 @@ impl Core {
 
         let (update_tx, update_rx) = mpsc::channel();
         let (rpc_tx, rpc_rx) = mpsc::channel();
+
         let stdout = process.stdout.unwrap();
+
         thread::spawn(move || for line in BufReader::new(stdout).lines() {
-                          if let Ok(data) = serde_json::from_slice::<Value>(line.unwrap()
-                                                                                .as_bytes()) {
-                              let req = data.as_object().unwrap();
-                              info!("<<< {:?}", req);
-                              if let (Some(id), Some(result)) = (req.get("id"), req.get("result")) {
-                                  rpc_tx.send((id.as_u64().unwrap(), result.clone()))
-                                        .unwrap();
-                                  info!(">>> {:?}", (id.as_u64().unwrap(), result.clone()));
-                              } else if let (Some(method), Some(params)) =
-                    (req.get("method"), req.get("params")) {
-                    let meth = method.as_str().unwrap();
-                    if meth == "set_style" || meth == "scroll_to" || meth == "update" {
-                        let request = json!([method.clone(), params.clone()]);
-                        update_tx.send(request).unwrap();
-                    } else {
-                        panic!("Unknown method {:?}.", method.as_str().unwrap());
-                    }
-                } else {
-                    panic!("Could not parse the core output: {:?}", req);
+            let line = line.unwrap();
+            info!("<<< {}", line);
+
+            if let Ok(data) = serde_json::from_slice::<Value>(line.as_bytes()) {
+                let req = data.as_object().unwrap();
+
+                if let (Some(id), Some(result)) = (req.get("id"), req.get("result")) {
+                    rpc_tx.send((id.as_u64().unwrap(), result.clone())).unwrap();
+                    continue;
                 }
-                          }
-                      });
+
+                if let (Some(method), Some(params)) = (req.get("method"), req.get("params")) {
+                    match method.as_str().unwrap() {
+                        "set_style" | "scroll_to" | "update" => {
+                            update_tx.send(json!([method, &params])).unwrap();
+                        }
+                        _ => {
+                            error!("Unknown method {:?}.", method.as_str().unwrap());
+                        }
+                    }
+                    continue;
+                }
+
+                error!("Unhandled core request");
+
+            } else {
+                error!("Could deserialize core output as a json object");
+            }
+        });
 
         let stderr = process.stderr.unwrap();
+
         thread::spawn(move || {
-                          let buf_reader = BufReader::new(stderr);
-                          for line in buf_reader.lines() {
-                              if let Ok(line) = line {
-                                  error!("[core] {}", line);
-                              }
-                          }
-                      });
+            let buf_reader = BufReader::new(stderr);
+            for line in buf_reader.lines() {
+                if let Ok(line) = line {
+                    error!("[core] {}", line);
+                }
+            }
+        });
 
         let stdin = process.stdin.unwrap();
 
@@ -83,10 +94,11 @@ impl Core {
             current_view: "".into(),
             views: HashMap::new(),
         };
+
         let view_id = core.new_view(Some(file.to_string()))
-                          .as_str()
-                          .unwrap()
-                          .to_string();
+            .as_str()
+            .unwrap()
+            .to_string();
         let view = View::new(file.to_string());
         core.views.insert(view_id.clone(), view);
         core.current_view = view_id;
@@ -172,7 +184,7 @@ impl Core {
     pub fn save(&mut self) {
         let views = self.views.clone();
         let save_params = json!({
-            "view_id": &self.current_view.clone(),
+            "view_id": &self.current_view.as_str(),
             "file_path": &views[&self.current_view].filepath.clone(),
         });
         self.request("save", save_params);
@@ -185,6 +197,7 @@ impl Core {
     pub fn left(&mut self) {
         self.call_edit("move_left", None);
     }
+
     pub fn left_sel(&mut self) {
         self.call_edit("move_left_and_modify_selection", None);
     }
@@ -192,6 +205,7 @@ impl Core {
     pub fn right(&mut self) {
         self.call_edit("move_right", None);
     }
+
     pub fn right_sel(&mut self) {
         self.call_edit("move_right_and_modify_selection", None);
     }
@@ -199,6 +213,7 @@ impl Core {
     pub fn up(&mut self) {
         self.call_edit("move_up", None);
     }
+
     pub fn up_sel(&mut self) {
         self.call_edit("move_up_and_modify_selection", None);
     }
@@ -206,6 +221,7 @@ impl Core {
     pub fn down(&mut self) {
         self.call_edit("move_down", None);
     }
+
     pub fn down_sel(&mut self) {
         self.call_edit("move_down_and_modify_selection", None);
     }
@@ -217,6 +233,7 @@ impl Core {
     pub fn page_up(&mut self) {
         self.call_edit("page_up", None);
     }
+
     pub fn page_up_sel(&mut self) {
         self.call_edit("page_up_and_modify_selection", None);
     }
@@ -251,6 +268,7 @@ impl Core {
     pub fn click(&mut self, line: u64, column: u64) {
         self.call_edit("click", Some(json!([line, column, 0, 1])));
     }
+
     pub fn drag(&mut self, line: u64, column: u64) {
         self.call_edit("drag", Some(json!([line, column, 0, 1])));
     }
@@ -267,40 +285,8 @@ impl Core {
             .map(|x| x.into())
             .unwrap()
     }
+
     pub fn paste(&mut self, s: String) {
         self.call_edit("insert", Some(json!({"chars": s})));
-    }
-
-    #[allow(dead_code)]
-    pub fn test(&mut self) {
-        self.render_lines(0, 10);
-    }
-
-    pub fn render_lines(&mut self, _start: u64, _end: u64) {
-        unimplemented!()
-        // self.rpc_index += 1;
-        // println!("render_lines");
-        // let value = ArrayBuilder::new()
-        //     .push("rpc")
-        //     .push_object(|builder| builder
-        //         .insert("index", self.rpc_index)
-        //         .insert_array("request", |builder| builder
-        //             .push("render_lines")
-        //             .push_object(|builder| builder
-        //                 .insert("first_line", _start)
-        //                 .insert("last_line", _end)
-        //             )
-        //         )
-        //     ).unwrap();
-        // self.write(value);
-    }
-
-    pub fn render_lines_sync(&mut self, _start: u64, _end: u64) -> Value {
-        unimplemented!()
-        // self.render_lines(_start, _end);
-        // let value = self.rpc_rx.recv().unwrap();
-        // let object = value.as_object().unwrap();
-        // assert_eq!(self.rpc_index, object.get("index").unwrap().as_u64().unwrap());
-        // object.get("result").unwrap().clone()
     }
 }

src/line.rs
@@ -1,51 +1,35 @@
-use serde_json;
+use std::default::Default;
 
-#[derive(Clone)]
+fn _return_true() -> bool {
+    true
+}
+
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 pub struct Line {
     pub text: Option<String>,
     pub cursor: Option<Vec<u64>>,
     pub styles: Option<Vec<i64>>,
+    #[serde(default="_return_true")]
+    #[serde(skip_deserializing)]
+    pub is_valid: bool,
+}
+
+impl Default for Line {
+    fn default() -> Line {
+        Line {
+            text: None,
+            cursor: None,
+            styles: None,
+            is_valid: true,
+        }
+    }
 }
 
 impl Line {
-    pub fn from_value(value: &serde_json::Value) -> Line {
-        let obj = value.as_object().unwrap();
-        let text = match obj.get("text") {
-            Some(text) => Some(text.as_str().unwrap().to_string()),
-            _ => None,
-        };
-        let cursor = match obj.get("cursor") {
-            Some(cursor) => {
-                Some(cursor.as_array()
-                           .unwrap()
-                           .iter()
-                           .map(|c| c.as_u64().unwrap())
-                           .collect())
-            }
-            _ => None,
-        };
-        let styles = match obj.get("styles") {
-            Some(styles) => {
-                Some(styles.as_array()
-                           .unwrap()
-                           .iter()
-                           .map(|s| s.as_i64().unwrap())
-                           .collect())
-            }
-            _ => None,
-        };
-        Line {
-            text: text,
-            cursor: cursor,
-            styles: styles,
-        }
-    }
-
     pub fn invalid() -> Line {
         Line {
-            text: Some("".into()),
-            cursor: None,
-            styles: None,
+            is_valid: false,
+            ..Default::default()
         }
     }
 }

src/main.rs
@@ -1,22 +1,28 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
-#![deny(clippy)]
+#![cfg_attr(feature="clippy", deny(clippy))]
+
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[macro_use]
+extern crate serde_json;
 
 #[macro_use]
 extern crate clap;
 #[macro_use]
 extern crate log;
 extern crate log4rs;
-#[macro_use]
-extern crate serde_json;
 extern crate termion;
 
 mod core;
 mod input;
 mod line;
-mod op;
-mod screen;
 mod update;
+mod operation;
+mod screen;
 mod view;
 
 use core::Core;
@@ -26,10 +32,10 @@ use screen::Screen;
 fn main() {
     log4rs::init_file("log_config.yaml", Default::default()).unwrap();
     let xi = clap_app!(xi =>
-        (about: "The Xi Editor")
-        (@arg core: -c --core +takes_value "Specify binary to use for the backend")
-        (@arg file: +required "File to edit")
-    );
+                       (about: "The Xi Editor")
+                       (@arg core: -c --core +takes_value "Specify binary to use for the backend")
+                       (@arg file: +required "File to edit")
+                      );
     let matches = xi.get_matches();
     let core_exe = matches.value_of("core").unwrap_or("xi-core");
     let file = matches.value_of("file").unwrap();

src/op.rs
@@ -1,95 +1,0 @@
-use serde_json;
-use std::str::FromStr;
-
-use line::Line;
-
-pub enum OpType {
-    Cpy,
-    Skip,
-    Invalidate,
-    Update,
-    Ins,
-}
-
-impl FromStr for OpType {
-    // FIXME(#28): we should have a custom error type
-    type Err = String;
-
-    fn from_str(op: &str) -> Result<Self, Self::Err> {
-        if op == "copy" {
-            Ok(OpType::Cpy)
-        } else if op == "skip" {
-            Ok(OpType::Skip)
-        } else if op == "invalidate" {
-            Ok(OpType::Invalidate)
-        } else if op == "update" {
-            Ok(OpType::Update)
-        } else if op == "ins" {
-            Ok(OpType::Ins)
-        } else {
-            Err(format!("Unknown Op type {:?}", op))
-        }
-    }
-}
-
-pub struct Op {
-    op: OpType,
-    n: u64,
-    lines: Option<Vec<Line>>,
-}
-
-impl Op {
-    pub fn from_value(value: &serde_json::Value) -> Op {
-        let obj = value.as_object().unwrap();
-        let lines = match obj.get("lines") {
-            Some(line_value) => {
-                let line_list = line_value.as_array().unwrap();
-                Some(line_list.iter()
-                              .map(|line| Line::from_value(line))
-                              .collect())
-            }
-            _ => None,
-        };
-        Op {
-            op: OpType::from_str(obj.get("op").unwrap().as_str().unwrap()).unwrap(),
-            n: obj.get("n").unwrap().as_u64().unwrap(),
-            lines: lines,
-        }
-    }
-
-    pub fn apply(&self, old_lines: &[Line], old_line_index: u64, new_lines: &mut Vec<Line>) -> u64 {
-        match self.op {
-            OpType::Cpy => {
-                let new_index = old_line_index + self.n;
-                for i in old_line_index..new_index {
-                    new_lines.push(old_lines[i as usize].clone());
-                }
-                new_index
-            }
-            OpType::Skip => old_line_index + self.n,
-            OpType::Invalidate => {
-                let new_index = old_line_index + self.n;
-                for _ in 0..self.n {
-                    new_lines.push(Line::invalid());
-                }
-                new_index
-            }
-            OpType::Update => {
-                let new_index = old_line_index + self.n;
-                let lines = self.lines.clone().unwrap();
-                for i in old_line_index..new_index {
-                    let mut line = old_lines[i as usize].clone();
-                    line.cursor = lines[i as usize].cursor.clone();
-                    line.styles = lines[i as usize].styles.clone();
-                    new_lines.push(line);
-                }
-                new_index
-            }
-            OpType::Ins => {
-                let lines = self.lines.clone().unwrap();
-                new_lines.extend(lines.iter().cloned());
-                old_line_index + self.n
-            }
-        }
-    }
-}

src/operation.rs
@@ -1,0 +1,126 @@
+use serde;
+use serde_json as json;
+use line::Line;
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub enum OperationType {
+    Copy_,
+    Skip,
+    Invalidate,
+    Update,
+    Insert,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub struct Operation {
+    #[serde(rename="op")]
+    #[serde(deserialize_with="deserialize_operation_type")]
+    pub operation_type: OperationType,
+    #[serde(rename="n")]
+    pub nb_lines: u64,
+    pub lines: Option<Vec<Line>>,
+}
+
+fn deserialize_operation_type<'de, D>(de: D) -> Result<OperationType, D::Error>
+    where D: serde::Deserializer<'de>
+{
+    let value: json::Value = try!(serde::Deserialize::deserialize(de));
+    match value {
+        json::Value::String(ref s) if &*s == "copy" => Ok(OperationType::Copy_),
+        json::Value::String(ref s) if &*s == "skip" => Ok(OperationType::Skip),
+        json::Value::String(ref s) if &*s == "invalidate" => Ok(OperationType::Invalidate),
+        json::Value::String(ref s) if &*s == "update" => Ok(OperationType::Update),
+        json::Value::String(ref s) if &*s == "ins" => Ok(OperationType::Insert),
+        _ => Err(serde::de::Error::custom("Unexpected value for operation type")),
+    }
+}
+
+impl Operation {
+    pub fn apply(&self, old_lines: &[Line], old_line_index: u64, new_lines: &mut Vec<Line>) -> u64 {
+        match self.operation_type {
+            OperationType::Copy_ => {
+                let new_index = old_line_index + self.nb_lines;
+                for i in old_line_index..new_index {
+                    new_lines.push(old_lines[i as usize].clone());
+                }
+                new_index
+            }
+            OperationType::Skip => old_line_index + self.nb_lines,
+            OperationType::Invalidate => {
+                let new_index = old_line_index + self.nb_lines;
+                for _ in 0..self.nb_lines {
+                    new_lines.push(Line::invalid());
+                }
+                new_index
+            }
+            OperationType::Update => {
+                let new_index = old_line_index + self.nb_lines;
+                let lines = self.lines.clone().unwrap();
+                for i in old_line_index..new_index {
+                    let mut line = old_lines[i as usize].clone();
+                    line.cursor = lines[i as usize].cursor.clone();
+                    line.styles = lines[i as usize].styles.clone();
+                    new_lines.push(line);
+                }
+                new_index
+            }
+            OperationType::Insert => {
+                let lines = self.lines.clone().unwrap();
+                new_lines.extend(lines.iter().cloned());
+                old_line_index + self.nb_lines
+            }
+        }
+    }
+}
+
+#[test]
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn deserialize_operation_from_value() {
+    use serde_json;
+
+    let value = json!({"n": 12, "op": "ins"});
+    let operation = Operation {
+        operation_type: OperationType::Insert,
+        nb_lines: 12,
+        lines: None,
+    };
+    let deserialized: Result<Operation, _> = serde_json::from_value(value);
+    assert_eq!(deserialized.unwrap(), operation);
+
+    let value = json!({"lines":[{"cursor":[0],"styles":[],"text":"foo"},{"styles":[],"text":""}],"n":60,"op":"invalidate"});
+    let operation = Operation {
+        operation_type: OperationType::Invalidate,
+        nb_lines: 60,
+        lines: Some(vec![
+            Line { cursor: Some(vec![0]), styles: Some(vec![]), text: Some("foo".to_owned()) },
+            Line { cursor: None, styles: Some(vec![]), text: Some("".to_owned()) }]),
+    };
+    let deserialized: Result<Operation, _> = serde_json::from_value(value);
+    assert_eq!(deserialized.unwrap(), operation);
+}
+
+#[test]
+fn deserialize_operation() {
+    use serde_json;
+
+    let s = r#"{"n": 12, "op": "ins"}"#;
+    let operation = Operation {
+        operation_type: OperationType::Insert,
+        nb_lines: 12,
+        lines: None,
+    };
+    let deserialized: Result<Operation, _> = serde_json::from_str(s);
+    assert_eq!(deserialized.unwrap(), operation);
+
+
+    let s = r#"{"lines":[{"cursor":[0],"styles":[],"text":"foo"},{"styles":[],"text":""}],"n":60,"op":"invalidate"}"#;
+    let operation = Operation {
+        operation_type: OperationType::Invalidate,
+        nb_lines: 60,
+        lines: Some(vec![
+            Line { cursor: Some(vec![0]), styles: Some(vec![]), text: Some("foo".to_owned()) },
+            Line { cursor: None, styles: Some(vec![]), text: Some("".to_owned()) }]),
+    };
+    let deserialized: Result<Operation, _> = serde_json::from_str(s);
+    assert_eq!(deserialized.unwrap(), operation);
+}

src/screen.rs
@@ -12,8 +12,9 @@ use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 use termion::raw::RawTerminal;
 
+use serde_json;
+
 use core::Core;
-use update::Update;
 use view::View;
 
 pub struct Screen {
@@ -61,20 +62,26 @@ impl Screen {
             let msg_list = msg.as_array().unwrap();
             let (method, params) = (msg_list[0].as_str().unwrap(),
                                     msg_list[1].as_object().unwrap());
-            if method == "update" {
-                let update = Update::from_value(params.get("update").unwrap());
-                core.update(&update);
+            match method {
+                "update" => {
+                    let update = serde_json::from_value(params.get("update").unwrap().clone())
+                        .unwrap();
+                    core.update(&update);
 
-                let view = core.view();
-                self.draw(&view);
-            } else if method == "scroll_to" {
-                let (col, line) = (params.get("col").unwrap().as_u64().unwrap(),
-                                   params.get("line").unwrap().as_u64().unwrap());
-                self.scroll(col, line);
-            } else if method == "set_style" {
-                // TODO(#26): ???
-            } else {
-                info!("Unknown request from backend {:?}", method);
+                    let view = core.view();
+                    self.draw(&view);
+                }
+                "scroll_to" => {
+                    let (col, line) = (params.get("col").unwrap().as_u64().unwrap(),
+                                       params.get("line").unwrap().as_u64().unwrap());
+                    self.scroll(col, line);
+                }
+                "set_style" => {
+                    // TODO:(#26): ???
+                }
+                _ => {
+                    info!("Unknown request from backend {:?}", method);
+                }
             }
         } else {
             thread::sleep(time::Duration::from_millis(10));

commit d60db44b7255f2d6c2e13f96256d3361ebf7ad4d

fixup
temporary fix for a bug introduced in
2d581e830dbd95de1c3fe8d66f0ca93b6bad9ba2

src/view.rs
@@ -18,9 +18,9 @@ impl View {
     }
 
     pub fn update(&mut self, update: &Update) {
-        if self.last_rev > update.rev {
-            return;
-        }
+        // if self.last_rev > update.rev {
+        //     return;
+        // }
 
         let mut lines = vec![];
         let mut index = 0;
@@ -29,7 +29,7 @@ impl View {
             index = operation.apply(&self.lines, index, &mut lines);
         }
 
-        self.last_rev = update.rev;
+        // self.last_rev = update.rev;
         self.lines = lines;
     }
 }