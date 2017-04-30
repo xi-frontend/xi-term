83bdcbd065f67b953372bfee6c7a439f2abca175
Corentin Henry <corentinhenry@gmail.com>
2017-04-30 16:14:11 -0700

    fix cursor position
    Draw cursors at the right position. This is not a proper fix though,
    since we draw "fake" cursors.
    
    This workaround is straight for https://github.com/IGI-111/xit

--- src/line.rs
+++ src/line.rs
@@ -1,5 +1,9 @@
 use std::default::Default;
 
+use termion::style;
+
+use errors::*;
+
 fn _return_true() -> bool {
     true
 }
@@ -30,6 +34,38 @@ impl Line {
         Line {
             is_valid: false,
             ..Default::default()
+        }
+    }
+
+    /// Insert the cursors and handle styles (not implemented yet)
+    pub fn render(&self) -> Result<String> {
+        let mut line = self.text
+                           .as_ref()
+                           .map(|s| s.clone())
+                           .unwrap_or(String::new());
+        self.pad(&mut line);
+        if let Some(cursors) = self.cursor.as_ref() {
+            let mut offset: usize = 0;
+            for idx in cursors {
+                let idx = offset + *idx as usize;
+                if idx + 1 > line.len() {
+                    error!("Cannot set cursor: cursor index {} is bigger than line length ({})", idx, line.len());
+                    bail!(ErrorKind::DisplayError);
+                }
+                line.insert_str(idx + 1, &format!("{}", style::Reset));
+                line.insert_str(idx, &format!("{}", style::Invert));
+                offset += 2;
+            }
+        }
+        Ok(line)
+    }
+
+    fn pad(&self, text: &mut String) {
+        if let Some('\n') = text.chars().last() {
+            text.pop();
+            text.push_str(" \n");
+        } else {
+            text.push(' ');
         }
     }
 }
