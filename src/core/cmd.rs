f38eacb9ceba1b6317a352b6a20faf4ed494ea7f
Bytebuddha <code@bytebuddha.rs>
2018-07-24 19:05:43 -0500

    Fix missing cmd module

---
+++ src/core/cmd.rs
@@ -0,0 +1,15 @@
+use xrl::ViewId;
+
+/// Command system for xi-term.
+/// A command represents a task the user wants the editor to preform,
+/// currently commands can only be input through the CommandPrompt. Vim style.
+#[derive(Debug)]
+pub enum Command {
+    /// Close the CommandPrompt
+    Cancel,
+    /// Quit editor.
+    Quit,
+    /// Save the current file buffer
+    Save(Option<ViewId>),
+    Invalid(String)
+}
